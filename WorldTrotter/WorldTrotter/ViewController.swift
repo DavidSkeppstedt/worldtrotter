b10e9117182529b832bd1a0f6bdc02ae7cd5c67f
David Skeppstedt <david.skeppstedt@gmail.com>
2016-01-24 21:27:17 +0100

    Initialized xcode project.

---
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by David Skeppstedt on 2016-01-24.
+//  Copyright © 2016 Dixum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
