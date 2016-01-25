39233a4f8eb7b8f2872b14c9f66c96d2d147a082
David Skeppstedt <david.skeppstedt@gmail.com>
2016-01-25 21:22:00 +0100

    Implemented view controller functionallity.

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -6,4 +6,57 @@
 //  Copyright © 2016 Dixum. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ConversionViewController : UIViewController {
+	//MARK: IBOutlets
+	@IBOutlet var celsiusLabel:UILabel!
+	@IBOutlet var textField:UITextField!
+
+	//MARK: conversion value properties
+	var fahrenheitValue:Double?{
+		didSet{
+			// will be changed as soon as the property changes value.
+			updateCelsiusLabel()
+		}
+	}
+	var celsiusValue:Double? {
+		if let value = fahrenheitValue {
+			return (value - 32) * (5/9)
+		}else {
+			return nil
+		}
+	}
+
+	//MARK: Number formatter property
+	let numberFormatter: NSNumberFormatter = {
+		let nf = NSNumberFormatter()
+		nf.numberStyle = .DecimalStyle
+		nf.minimumFractionDigits = 0
+		nf.maximumFractionDigits = 1
+		return nf
+	}()
+
+	func updateCelsiusLabel() {
+		if let value = celsiusValue {
+			celsiusLabel.text = numberFormatter.stringFromNumber(value)
+		} else {
+			celsiusLabel.text = "???"
+		}
+	}
+
+
+	//MARK: IBAction
+	@IBAction func fahrenheitFieldEditingChanged(textField:UITextField) {
+
+		if let text = textField.text ,value = Double(text){
+			fahrenheitValue = value
+		}else {
+			fahrenheitValue = nil
+		}
+	}
+
+	@IBAction func dismissKeyboard(send:AnyObject) {
+		textField.resignFirstResponder()
+	}
+}
