0b6a621dc1109ba3a94a35c8aa11dd0182ffd921
David Skeppstedt <david.skeppstedt@gmail.com>
2016-01-25 21:21:23 +0100

    Deleted unclear viewcontroller and added a new more descriptive one.

---
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by David Skeppstedt on 2016-01-25.
+//  Copyright © 2016 Dixum. All rights reserved.
+//
+
+import Foundation
