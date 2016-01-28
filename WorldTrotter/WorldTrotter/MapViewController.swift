dccccdbbf14d84aa9f5049f4fa128686e28da297
David Skeppstedt <david.skeppstedt@gmail.com>
2016-01-28 20:33:08 +0100

    Attached a viewcontroller class to the map view. Yay!

---
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by David Skeppstedt on 2016-01-28.
+//  Copyright © 2016 Dixum. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController:UIViewController {
+	override func viewDidLoad() {
+		print("Hey i am here")
+	}
+}
