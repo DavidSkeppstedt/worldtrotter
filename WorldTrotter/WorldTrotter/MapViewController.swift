aa63ac8d14e63b48c21683ff80f857d2953d630f
David Skeppstedt <david.skeppstedt@gmail.com>
2016-01-28 21:20:14 +0100

    Implemented nice constrains for the segmentcontroller and also made it clickable. It even responds with accordingly.

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -15,6 +15,38 @@ class MapViewController:UIViewController {
 		//this creates a new mapview and sets it as the root view.
 		map = MKMapView()
 		self.view = map
+
+		let segmentController = UISegmentedControl(items: ["Standard","Hybrid","Satellite"])
+		segmentController.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+		segmentController.selectedSegmentIndex = 0
+		segmentController.translatesAutoresizingMaskIntoConstraints = false
+
+		self.view.addSubview(segmentController)
+
+		let topConstrian = segmentController.topAnchor.constraintEqualToAnchor(self.topLayoutGuide.bottomAnchor,constant: 8)
+		let margins = view.layoutMarginsGuide
+		let leadingConstrian = segmentController.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor)
+		let trailingConstrian = segmentController.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor)
+
+		topConstrian.active = true
+		leadingConstrian.active = true
+		trailingConstrian.active = true
+
+		segmentController.addTarget(self, action: "mapTypeChanged:", forControlEvents: .ValueChanged)
+
+	}
+
+	func mapTypeChanged(segControl:UISegmentedControl) {
+		switch segControl.selectedSegmentIndex {
+		case 0:
+			map.mapType = .Standard
+		case 1:
+			map.mapType = .Hybrid
+		case 2:
+			map.mapType = .Satellite
+		default:
+			break
+		}
 	}
 
 	override func viewDidLoad() {
